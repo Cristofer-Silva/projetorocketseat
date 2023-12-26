9f1301dc34c424a241799836b95d88d4f5a52433
Cristofer Silva <cristofersilva.rp@gmail.com>
2023-12-26 18:54:25 -0300

    alterações finais do curso

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function toggleMode() {
 
     const img = document.querySelector("#profile img")
 
-    if(html.classList.contains('light')){
+   /* para mudar a foto, use o código:
+   
+   if(html.classList.contains('light')){
         img.setAttribute('src', './assets/avatar.png')
     } else{
         img.setAttribute('src', './assets/avatar-whitebg.png')
     }
+    */
 }
